bfc8736a31cf299b41cdf7eb5e1a409ba3f615a2
Caleb <caleb.eby01@gmail.com>
2017-09-19 19:56:00 +0000

    Inject DATO locals

--- app.js
+++ app.js
@@ -4,21 +4,39 @@ const jsStandards = require('spike-js-standards')
 const pageId = require('spike-page-id')
 const sugarml = require('sugarml')
 const sugarss = require('sugarss')
+const SpikeDatoCMS = require('spike-datocms')
+
 const env = process.env.SPIKE_ENV
+
+const locals = {}
 
 module.exports = {
   devtool: 'source-map',
   matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
-  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', '.c9/**/*'],
+  
   reshape: htmlStandards({
     parser: sugarml,
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    locals: (ctx) => {
+      locals.pageId = pageId(ctx)
+      return locals
+    },
     minify: env === 'production'
   }),
+  
   postcss: cssStandards({
     parser: sugarss,
     minify: env === 'production',
     warnForDuplicates: env !== 'production'
   }),
-  babel: jsStandards()
+  
+  babel: jsStandards(),
+  
+    plugins: [
+    new SpikeDatoCMS({
+      addDataTo: locals,
+      token: '2e9b95b0328903c6d9acdf25665510',
+      models: []
+    })
+  ]
 }
